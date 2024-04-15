5f8c045ff9dcb6467bf0de9610490fb6b890c3de
Nicolás Fernández <fernandeznicolas0505@gmail.com>
2024-04-14 22:35:47 -0400

    form login

---
+++ src/user/login.jsx
@@ -0,0 +1,29 @@
+function login(){
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Email:
+                <input
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    required
+                />
+            </label>
+            <label>
+                Password:
+                <input
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                />
+            </label>
+            <input type="submit" value="Enviar" />
+        </form>
+    )
+}
+
+export default login()
