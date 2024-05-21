0dd7e5b144d02a64c145751b31970781e428fd31
Nicolás Fernández <fernandeznicolas0505@gmail.com>
2024-05-21 18:07:06 -0400

    visual changes

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -4,7 +4,9 @@ function LandingPage(){
     return(<div>
 
         <img className="imgCenter" src="https://res.cloudinary.com/darhaqq0v/image/upload/v1713117225/pngwing.com_1_i3u0yc.png" alt="avion" />
-        <h1>Aerolinea 28</h1>
+        <h1>Aerolineas 28</h1>
+        <br></br>
+        <p>Enamorate de viajar</p>
         </div>)
 }
 
