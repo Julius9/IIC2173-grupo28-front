e110eb239b91157db734389e8e5e7d9c6c01e4b7
Nicolás Fernández <fernandeznicolas0505@gmail.com>
2024-05-22 15:02:17 -0400

    purchase-completed

--- src/compras/purchaseCompleted.jsx
+++ src/compras/purchaseCompleted.jsx
@@ -1,5 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link, useSearchParams } from "react-router-dom";
+import axios from "axios";
+
+
 function CompraCompletada() {
+
+    const [searchParams] = useSearchParams();
+    const [data, setData] = useState(null)
+
+    const commitTransaction = async (event) => {
+        console.log(localStorage.getItem('token'))
+        event.preventDefault();
+
+
+        axios.post(`https://api.legitapp.org/transaction/commit`, {
+
+            ws_token: searchParams.get('token_ws') || '',
+            request_id: searchParams.get('request_id') || ''
+
+
+        }, {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        }).then(
+            (response) => {
+                console.log('Response body:', response.data);
+                setData(response.data)
+
+            }
+        ).catch((error) => {
+            console.error('Ocurrió un error CT:', error);
+            //console.log(error.response.data.errors[0].message)
+            //console.log((error.response.data['errors']))
+
+
+        });
+    }
+
+
+
+
+    if (isLoading) {
+        return (
+            <div className="p-20">
+                <h1>Loading...</h1>
+            </div>
+        )
+    }
+
+    console.log(data)
+
+    return (
+        <div className="p-8 mt-20 flex flex-col gap-3 w-1/3 mx-auto rounded-xl shadow-[0_0px_8px_#b4b4b4]">
+            <h1 className="text-center">Purchase Completed</h1>
+            <p>{data.message}</p>
+            <Link to="/" className="bg-black text-white px-3 py-2 rounded text-center">Volver a inicio</Link>
+        </div>
+    );
 
 }
 
